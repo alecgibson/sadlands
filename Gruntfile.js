fc94f5f41a13c2053a63f2480befe1b64559afa2
Alec Gibson <Alexander.Gibson@softwire.com>
2017-05-11 07:36:05 +0100

    Move stuff for handlebars

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,16 @@
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-static-handlebars');
-
     grunt.initConfig({
         staticHandlebars: {
             options: {
                 // Task-specific options go here.
+            },
+            files: {
+                'compiled/**/*.html':'source/**/*.hbt'
             }
         }
     });
+
+    grunt.loadNpmTasks('grunt-static-handlebars');
+
+    grunt.registerTask('default', ['staticHandlebars']);
 };
