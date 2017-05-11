6e49f8d08561dbc68e0fb476289368e1711dc1de
Alec Gibson <Alexander.Gibson@softwire.com>
2017-05-11 08:31:41 +0100

    Add Sadlands pages to Nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
+var clean = require('gulp-clean');
 
-gulp.task('default', function () {
-    return gulp.src('src/pages/**/*.html')
+gulp.task('clean', function() {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('nunjucks', ['clean'], function() {
+    return gulp.src('src/pages/**/*.+(html|nunjucks)')
         .pipe(nunjucksRender({
             path: ['src/templates'] // String or Array
         }))
         .pipe(gulp.dest('dist'));
 });
+
+gulp.task('copy', ['clean'], function() {
+    return gulp.src('src/assets/**/*')
+        .pipe(gulp.dest('dist/assets'));
+});
+
+gulp.task('default', ['clean', 'nunjucks', 'copy']);
