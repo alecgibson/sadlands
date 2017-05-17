0d0baa6c3efd1e3067be33d8cc53efd543631491
Alec Gibson <Alexander.Gibson@softwire.com>
2017-05-17 07:31:43 +0100

    Improve anchor links and update shop copy

--- src/assets/javascript/smooth-scroll.js
+++ src/assets/javascript/smooth-scroll.js
@@ -1,14 +1,25 @@
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top - 120
-        }, 1000);
-        return false;
-      }
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                scrollTo(target, 1000);
+                return false;
+            }
+        }
+    });
+
+    if (window.location.hash) {
+        var target = $(window.location.hash);
+        if (target.length) {
+            scrollTo(target, 100);
+        }
     }
-  });
+
+    function scrollTo(target, duration) {
+        $('html, body').animate({
+            scrollTop: target.offset().top - 120
+        }, duration);
+    }
 });
